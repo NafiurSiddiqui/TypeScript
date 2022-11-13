b55d97e5ec5c6dff3e3a0d022fd7ffb47f30f5c6
Nafi R <saejim91@gmail.com>
2022-11-13 22:19:24 +0600

    finished Config chapter

--- 2 - config/app.ts
+++ 2 - config/app.ts
@@ -1,3 +1,9 @@
-const shoutOut = 'Eddie';
+//With StrictNullChecks, We will get error if there is no element(button in this case) exist on the DOM.
+// To work around this, we either need to put a IF check or use ! at the end of the DOM selector. 👇
 
-console.log(shoutOut);
+// const button = document.querySelector('button');
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => {
+	console.log('Clicked!');
+});
