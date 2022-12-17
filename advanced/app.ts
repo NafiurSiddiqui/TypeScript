29720879750de2e75cc4eb022d3f56cb4f638aaa
Nafi R <saejim91@gmail.com>
2022-12-17 23:12:23 +0600

    Advanced types: Type Guards

--- advanced/app.ts
+++ advanced/app.ts
@@ -1,7 +1,7 @@
 //-------------- INTERSECTION
 
-
-//EXAMPLE: 1
+/**
+ * //EXAMPLE: 1
 
 type Admin = {
 	name: string;
@@ -29,3 +29,53 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+ * 
+ */
+
+//------------- Type Guards
+
+type Admin = {
+	name: string;
+	privilges: string[];
+};
+
+type Employee = {
+	name: string;
+	startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+//SCENE 1
+function add(a: Combinable, b: Combinable) {
+	if (typeof a === 'string' || typeof b === 'string') {
+		return a.toString() + b.toString();
+	}
+
+	return a + b;
+}
+
+//SCENE 2
+
+//we may have a scene where we can't type guard with typeof, like in the scenario of an object check. We can not directly access the properties for check, nor we can do typeof === 'object'. in that case, the following hack will work
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+	console.log(`Name: ${emp.name}`);
+	// console.log(`Privileges: ${emp.privileges}`);
+	//👆 this will not work
+
+	if ('privileges' in emp) {
+		console.log(`Privileges: ${emp.privileges}`);
+	}
+
+	if ('startDate' in emp) {
+		console.log(`Start Date: ${emp.startDate}`);
+	}
+}
