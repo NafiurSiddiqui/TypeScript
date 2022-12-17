a9e3fc4228a818b93d412e7dfd9f10ed9db5685b
Nafi R <saejim91@gmail.com>
2022-12-17 22:50:53 +0600

    Advanced types: Intersection

--- advanced/app.ts
+++ advanced/app.ts
@@ -1,1 +1,31 @@
 //-------------- INTERSECTION
+
+
+//EXAMPLE: 1
+
+type Admin = {
+	name: string;
+	privilges: string[];
+};
+
+type Employee = {
+	name: string;
+	startDate: Date;
+};
+
+
+type ElevatedEmployee = Admin & Employee;
+
+
+const e1: ElevatedEmployee = {
+    name: "Timmy",
+    privilges: ['create-server'],
+    startDate: new Date();
+}
+
+//EXAMPLE 2
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
